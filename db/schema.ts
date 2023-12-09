e9dd0ff5671b21b7b40ed25aaae194a34226853f
karmakarmeghdip <karmakarmeghdip@gmail.com>
2023-12-09 17:23:41 +0530

    schema done

---
+++ db/schema.ts
@@ -0,0 +1,31 @@
+// import { sql } from "drizzle-orm";
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+
+const users = sqliteTable("Users", {
+  id: integer("id").primaryKey().unique(),
+  name: text("name"),
+  token: text("token"),
+  phnNo: text("phnNo"),
+  gender: text("gender"),
+  age: integer("age"),
+  location: text("location")
+})
+
+const reports = sqliteTable("Reports", {
+  id: integer("id").primaryKey().unique(),
+  user_id: integer("user_id").references(() => users.id),
+  description: text("description"),
+  image_url: text("image_url"),
+  type: text("type"),
+  location: text("location"),
+  address: text("address"),
+  time: integer("time"),
+  disaster_id: integer("disaster_id").references(() => disasters.id)
+})
+
+const disasters = sqliteTable("Disasters", {
+  id: integer("id").primaryKey().unique(),
+  avg_loc: text("avg_loc"),
+  address: text("address"),
+  status: text("status")
+})
